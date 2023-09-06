0c4fea0496bb6a25766cd6756fa6d6b4e78a4a4f
domparso <domparso@hotmail.com>
2023-09-07 00:23:37 +0800

    modify user LOG no display detial

--- web/src/components/LogsTable.js
+++ web/src/components/LogsTable.js
@@ -341,7 +341,11 @@ const LogsTable = () => {
                     <Table.Cell>{log.prompt_tokens ? log.prompt_tokens : ''}</Table.Cell>
                     <Table.Cell>{log.completion_tokens ? log.completion_tokens : ''}</Table.Cell>
                     <Table.Cell>{log.quota ? renderQuota(log.quota, 6) : ''}</Table.Cell>
-                    <Table.Cell>{log.content}</Table.Cell>
+                    {
+                        isAdminUser && (
+                            <Table.Cell>{log.content}</Table.Cell>
+                        )
+                    }
                   </Table.Row>
                 );
               })}
